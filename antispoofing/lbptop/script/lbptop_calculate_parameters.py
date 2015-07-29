52d25521da1c9a0ab7a3d156f8afd7cfb19ef44d
Ivana Chingovska <ivana.chingovska@idiap.ch>
2015-07-29 12:00:30 +0200

    adding option to work with MSU db

--- antispoofing/lbptop/script/lbptop_calculate_parameters.py
+++ antispoofing/lbptop/script/lbptop_calculate_parameters.py
@@ -2,7 +2,7 @@
 #Tiago de Freitas Pereira <tiagofrepereira@gmail.com>
 #Fri Jul 13 14:30:00 CEST 2012
 
-"""Calculates the LBPTop planes (XY,XT,YT) of the normalized faces in the videos in the REPLAY-ATTACK database. This code extract the LBP-TOP features using the MULTIRESOLUTION approach, setting more than one value for Rt. The result is the LBP histogram over all orthogonal frames of the video (XY,XT,YT). Different types of LBP operators are supported. The histograms can be computed for a subset of the videos in the database (using the protocols in REPLAY-ATTACK). The output is a single .hdf5 file for each video. The procedure is described in the paper: "LBP-TOP based countermeasure against facial spoofing attacks" - de Freitas Pereira, Tiago and Anjos, Andre and De Martino, Jose Mario and Marcel, Sebastien; ACCV - LBP 2012
+"""Calculates the LBPTop planes (XY,XT,YT) of the normalized faces in the videos in the REPLAY-ATTACK and CASIA-FASD and MSU-MFSD database. This code extract the LBP-TOP features using the MULTIRESOLUTION approach, setting more than one value for Rt. The result is the LBP histogram over all orthogonal frames of the video (XY,XT,YT). Different types of LBP operators are supported. The histograms can be computed for a subset of the videos in the database (using the protocols in REPLAY-ATTACK). The output is a single .hdf5 file for each video. The procedure is described in the paper: "LBP-TOP based countermeasure against facial spoofing attacks" - de Freitas Pereira, Tiago and Anjos, Andre and De Martino, Jose Mario and Marcel, Sebastien; ACCV - LBP 2012
 
 """
 
@@ -17,6 +17,7 @@ from .. import spoof
 import antispoofing.utils
 import string
 
+import antispoofing.utils.faceloc as faceloc
 from antispoofing.utils.faceloc import *
 from antispoofing.lbptop.helpers import *
 from antispoofing.utils.db import *
@@ -37,7 +38,7 @@ def main():
 
   parser.add_argument('output_dir', metavar='DIR', default=OUTPUT_DIR, help="This path will be prepended to every file output by this procedure (defaults to '%(default)s')")
 
-  parser.add_argument('-n', '--normface-size', dest="normfacesize", default=64, type=int, help="this is the size of the normalized face box if face normalization is used (defaults to '%(default)s')")
+  parser.add_argument('-n', '--normface-size', dest="normfacesize", default=[64,], type=int, help="this is the size of the normalized face box if face normalization is used. If more values are given, then the first one corresponds to height, and the second to the width (defaults to '%(default)s')", nargs='+')
 
   parser.add_argument('--ff', '--facesize_filter', dest="facesize_filter", default=50, type=int, help="all the frames with faces smaller then this number, will be discarded (defaults to '%(default)s')")
 
@@ -72,8 +73,16 @@ def main():
 
   parser.add_argument('-p', '--all-planes', action='store_true', default=False, dest='all_planes', help='Save 5 planes in different files (XY, XT, YT, XY-YT, XY-XT-YT), otherwise will save only the XY-XT-YT (defaults to "%(default)s")')
 
+  parser.add_argument('--bbx', '--boundingbox', action='store_true', default=False, dest='boundingbox', help='If True, will read the face locations using the bbx function of the File class of the database. If False, will use faceloc.read_face utility to read the faceloc. For MSU-MFSD only (defaults to "%(default)s")')
+  parser.add_argument('--nn', '--nonorm', dest='nonorm', action='store_true', default=False, help='If True, normalization on the bounding box will NOT be perfomed. If False, normalization will be done depending on the -n parameter.')
+ 
   # For SGE grid processing @ Idiap
   parser.add_argument('--grid', dest='grid', action='store_true', default=False, help=argparse.SUPPRESS)
+
+  # The next option just returns the total number of cases we will be running
+  # It can be used to set jman --array option. To avoid user confusion, this
+  # option is suppressed # from the --help menu
+  parser.add_argument('--grid-count', dest='grid_count', action='store_true', default=False, help=argparse.SUPPRESS)
 
   #######
   # Database especific configuration
@@ -88,7 +97,13 @@ def main():
   inputDir  = args.input_dir
   directory = args.output_dir
 
-  normfacesize    = args.normfacesize
+  if args.nonorm == True: # the frames should not be normalized
+    normfacesize = None
+  elif len(args.normfacesize) == 1: # just one values is given
+    normfacesize = [args.normfacesize[0], args.normfacesize[0]]
+  else:  
+    normfacesize    = args.normfacesize
+
   facesize_filter = args.facesize_filter
 
   nXY = args.nXY
@@ -129,6 +144,10 @@ def main():
     realObjects, attackObjects = database.get_all_data()
     process = realObjects + attackObjects 
 
+  if args.grid_count:
+    print len(process)
+    sys.exit(0)
+
   # finally, if we are on a grid environment, just find what I have to process.
   if args.grid:
     key = int(os.environ['SGE_TASK_ID']) - 1
@@ -154,9 +173,18 @@ def main():
     #Loading the face locations
     if string.find(database.short_description(), "CASIA") != -1:
       flocfile = obj.facefile()
-    else:
-      flocfile = obj.facefile(inputDir)
-    locations = preprocess_detections(flocfile,input.number_of_frames,facesize_filter=facesize_filter)
+      locations = preprocess_detections(flocfile,input.number_of_frames,facesize_filter=facesize_filter)
+    elif string.find(database.short_description(), "MSU") != -1:
+      if args.boundingbox: # load the locations based on the bbx function of the File object
+        locations = obj.bbx(directory=args.input_dir)
+        locations = {x[0]:faceloc.BoundingBox(x[1], x[2], x[3], x[4]) for x in locations} # for MSU MFSD
+        locations = faceloc.expand_detections(locations, input.number_of_frames)
+      else:  
+        facefile = obj.facefile(args.input_dir)
+        locations = preprocess_detections(facefile, len(input), facesize_filter=args.min_face_size)
+    else:    
+      flocfile = obj.facefile(args.input_dir)
+      locations = preprocess_detections(flocfile,input.number_of_frames,facesize_filter=facesize_filter)
 
     sys.stdout.write("Processing file %s (%d frames) [%d/%d] " % (filename,
       input.number_of_frames, index+1, len(process)))
@@ -170,6 +198,8 @@ def main():
     grayFrames = numpy.zeros(shape=(nFrames,vin.shape[2],vin.shape[3]))
     for i in range(nFrames):
       grayFrames[i] = bob.ip.color.rgb_to_gray(vin[i,:,:,:])
+      if string.find(database.short_description(), "MSU") != -1 and obj.is_rotated(): # rotate the frame by 180 degrees if needed
+        grayFrames[i] = numpy.rot90(numpy.rot90(grayFrames[i]))
       if(tan_triggs):
         grayFrames[i] = tantriggs(grayFrames[i])
 
